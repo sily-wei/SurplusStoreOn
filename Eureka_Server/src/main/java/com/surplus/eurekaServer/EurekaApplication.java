8a6b224408e8303251f1ae79c29e44531907b44b
sily-wei <sily_wei@163.com>
2020-01-10 20:05:09 +0800

    搭建了项目的第一版框架

--- Eureka_Server/src/main/java/com/surplus/eurekaServer/EurekaApplication.java
+++ Eureka_Server/src/main/java/com/surplus/eurekaServer/EurekaApplication.java
@@ -1,8 +1,15 @@
 package com.surplus.eurekaServer;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @author weining
  * @date 2020/1/10 19:45
  */
+@SpringBootApplication
 public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class,args);
+    }
 }
